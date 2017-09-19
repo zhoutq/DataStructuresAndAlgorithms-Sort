f0fd20cf10faa6d6d0a4e146d4b01ea2e6831408
周统乾 <zhoutongqian@feinno.com>
2017-09-19 13:21:06 +0800

    半折插入注释

--- SortTest/SortTest/main.c
+++ SortTest/SortTest/main.c
@@ -83,28 +83,31 @@ void binaryInsertSort(int a[], int n)
 {
     for (int i = 1; i < n; i++) {
         
-        int low=0,high=i;
+        int low = 0,high = i;
         
         int num = a[i];        //复制为哨兵，即存储待排序元素
-        while (low <= high) {  //查找在有序表的插入位置  (遍历表)
+        while (low <= high) {  //查找在 有序表 的插入位置  (遍历表)
             
-            int m = (low + high) / 2;
-            if(num<a[m]) {
+            int m = (low + high) / 2; // 取有序表中间位置
+            if (num < a[m]) { // 如果待插入值小于中间值，则继续在前半部分查找，即 high 为 m - 1;
                 
                 high = m - 1;
             }
-            else {
+            else { // 否则，在后半部分查找，即 low 为 m + 1
                 
                 low = m + 1;
             }
             
         }
         
-        int j = i-1;
-        for (j = i-1; j >= high + 1; j--) {
+        int j;
+        // 待插位置之后的元素后移
+        for (j = i - 1; j >= low; j--) { // j:有序表最后一位，i:无序表第一位。low:待插的位置。>= low 的都往后移动。最后插入到low的位置
+            
             a[j + 1] = a[j];
         }
-        a[j+1] = num;      //插入到正确位置
+        
+        a[j+1] = num;      //插入到正确位置， j + 1 是因为上个循环 j-- 了
     }
     
 }
@@ -228,18 +231,20 @@ void merge(int a[], int low, int mid, int high) {
     free(temp);
 }
 
+
 int main(int argc, const char * argv[]) {
     
+    int n = 10;
     int a[10] = {2,3,1,6,9,1,4,8,5,7};
 //    int a[10] = {2,2,2,2,2,1,1,1,1,1};
     
-    printfSort(a, 10);
-//    bubbleSort(a, 10);
-//    insertSort(a, 10);
-//    binaryInsertSort(a, 10);
-//    selectSort(a, 10);
-//    quickSort(a, 0, 9);
-    mergeSort(a, 0, 9);
-    printfSort(a, 10);
+    printfSort(a, n);
+//    bubbleSort(a, n);
+//    insertSort(a, n);
+    binaryInsertSort(a, n);
+//    selectSort(a, n);
+//    quickSort(a, 0, n - 1);
+//    mergeSort(a, 0, n - 1);
+    printfSort(a, n);
     return 0;
 }
